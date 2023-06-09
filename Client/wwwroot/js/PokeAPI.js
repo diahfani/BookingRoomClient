28a60532aaace8563ef6983bf9ba054c8a134c58
diahfani <diahaufaarini5@gmail.com>
2023-06-09 21:19:30 +0700

    Add project files.

---
+++ Client/wwwroot/js/PokeAPI.js
@@ -0,0 +1,122 @@
+﻿$.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon",
+    type: 'GET',
+}).done((result) => {
+    let temp = "";
+    $.each(result.results, (key, val) => {
+        temp += `<tr>
+            <td>${key + 1}</td>
+            <td>${val.name}</td>
+            <td><button type="button" onclick="getDetail('${val.url}')" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Detail</button></td>
+            </tr>`
+    })
+    /*$(".modal-title").html(val.name);*/
+    $("tbody").append(temp);
+    console.log(result)
+})
+
+function getDetail(Url) {
+    $.ajax({
+        url: Url,
+        type: 'GET',
+    }).done((result) => {
+        console.log(result);
+        $("#imgPoke").attr("src", result.sprites.other['official-artwork'].front_default);
+        $("#imgPoke").attr("width", 400);
+        let temp = "";
+        $.each(result.types, (key, val) => {
+            const badgeColor = {
+                grass: "success",
+                fire: "danger",
+                poison: "warning",
+                normal: "secondary",
+                flying: "dark",
+                water: "primary",
+                bug: "info",
+            }[val.type.name] || "light text-dark";
+            temp += `<span class="badge bg-${badgeColor} badge-width">${val.type.name}</span>`
+        })
+        $("#rowBadge").html(temp);
+        $(".name-pokemon").html(result.name)
+
+        $("#hp").attr({
+            value: result.stats[0].base_stat,
+            style: `--value: ${result.stats[0].base_stat}; --max: 200;`
+        })
+        $("#attack").attr({
+            value: result.stats[1].base_stat,
+            style: `--value: ${result.stats[1].base_stat}; --max: 200;`
+        })
+        $("#defense").attr({
+            value: result.stats[2].base_stat,
+            style: `--value: ${result.stats[2].base_stat}; --max: 200;`
+        })
+        $("#specialAttack").attr({
+            value: result.stats[3].base_stat,
+            style: `--value: ${result.stats[3].base_stat}; --max: 200;`
+        })
+        $("#specialDefense").attr({
+            value: result.stats[4].base_stat,
+            style: `--value: ${result.stats[4].base_stat}; --max: 200;`
+        })
+        $("#speed").attr({
+            value: result.stats[5].base_stat,
+            style: `--value: ${result.stats[5].base_stat}; --max: 200;`
+        })
+        let abilities = ""
+        let arrayUrl = [{}]
+
+        $.each(result.abilities, (key,val) => {
+            abilities += `<h5 class="name-abilities mt-2" id="name${key}">${val.ability.name}</h5>`
+            arrayUrl.push({
+                "key": key,
+                "url": val.ability.url,
+                "name": val.ability.name
+            })
+
+        })
+        arrayUrl.shift()
+        $(".abilities-0").html(abilities);
+        getAbilitiesDetail(arrayUrl)
+        getDescription(result.species.url)
+        $(".base-experience").html(`Base Experience : ${result.base_experience}`)
+        $(".height").html(`Height : ${ result.height }`)
+        $(".weight").html(`Weight :${result.weight}`)
+    })
+}
+
+function getAbilitiesDetail(arrayUrl) {
+    $.each(arrayUrl, (key, val) => {
+
+        $.ajax({
+            url: val.url,
+            type: 'GET',
+        }).done((result) => {
+            $.each(result.effect_entries, (key1, val) => {
+            if (val.language.name == 'en') {
+                $(`<p class="desc-abilities" id="desc${key}">${val.effect}</p>`).insertAfter(`#name${key}`)
+            }
+        })
+        })
+    })
+}
+
+function getDescription(url) {
+    /*console.log(url)*/
+    $.ajax({
+        url: url,
+        type: 'GET',
+    }).done((result) => {
+        console.log(result)
+        $.each(result.flavor_text_entries, (key, val) => {
+            if (val.language.name == 'en') {
+                console.log(val.flavor_text)
+            $(".description").html(val.flavor_text)
+            }
+        })
+    })
+}
+
+$(document).ready(function () {
+    $("#table1").DataTable();
+})
---
+++ Client/wwwroot/js/site.js
@@ -0,0 +1,132 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+/*let col1row1 = document.querySelector(".column1");
+let col2row1 = document.querySelector(".baris2");
+let col3row1 = document.querySelector(".baris3");
+
+*//*document.getElementById("button1").addEventListener("click", myFunction1);*//*
+document.getElementById("button1").addEventListener("click", function () {
+    col1row1.style.backgroundColor = "lightblue";
+    col1row1.style.color = "steelblue";
+    col2row1.style.backgroundColor = "darkseagreen";
+    col2row1.style.color = "green";
+    col3row1.style.backgroundColor = "beige";
+    col3row1.style.color = "yellowgreen";
+})
+
+col2row1.addEventListener("mouseover", function () {
+    col1row1.style.backgroundColor = "darkseagreen";
+    col1row1.style.color = "green";
+    col2row1.style.backgroundColor = "beige";
+    col2row1.style.color = "yellowgreen";
+    col3row1.style.backgroundColor = "lightblue";
+    col3row1.style.color = "steelblue";
+})
+
+col2row1.addEventListener("click", function () {
+})
+
+const animals = [
+    { name: "nemo", species: "fish", class: { name: "invertebrata" } },
+    { name: "gary", species: "mouse", class: { name: "mamalia" } },
+    { name: "dory", species: "fish", class: { name: "invertebrata" } },
+    { name: "tom", species: "mouse", class: { name: "mamalia" } },
+    { name: "aji", species: "wibu", class: { name: "mamalia" } }
+]*/
+
+/*const onlyMouse = animals.map(animal => {
+    if (animal.species == "mouse") {
+        return animal;
+    }
+});*/
+/*const onlyMouse = [];
+animals.forEach(animal => {
+    if (animal.species != "mouse") {
+        animal.class.name = "non mamalia";
+    }
+    else {
+        onlyMouse.push(animal);
+    }
+})*/
+
+/*for(let i = 0; i < animals.length; i++){
+    *//*console.log(animals[i].name);*//*
+    if (animals[i].species != "mouse") {
+        animals[i].class.name = "non mamalia";
+    } else {
+        onlyMouse.push(animals[i]);
+    }
+    
+};*/
+
+/*console.log(animals);
+console.log(onlyMouse);
+*/
+/*
+$.ajax({
+    url: "https://swapi.dev/api/people/ ",
+}).done((result) => {
+    if (result == null) {
+        let temp = "<div class='alert alert-danger'>Data tidak ditemukan</div>";
+    }
+    let temp = "";
+    let number = 0;
+    $.each(result.results, (key, val) => {
+        number++
+        temp += "<tr>" + 
+            "<td>" + number + "</td>" +
+            "<td>" + val.name + "</td>" +
+            "<td>" + val.height + "</td>" +
+            "<td>" + val.mass + "</td>" +
+            "<td>" + val.hair_color + "</td>" +
+            "<td>" + val.gender + "</td>" +
+            "</tr>";        
+    })
+$("tbody").append(temp);
+})*/
+
+/*escape character string pake backslash (\)*/
+/*literal template (``)*/
+/*radar chart*/
+/*
+$.ajax({
+    url: "https://swapi.dev/api/people/ ",
+    type: 'GET',
+    beforeSend: function () {
+        $("#loader").show();
+    },
+    success: function (result) {
+        $("#loader").hide();
+        let temp = "";
+        let number = 0;
+        $.each(result.results, (key, val) => {
+            number++
+            temp += "<tr>" +
+                "<td>" + number + "</td>" +
+                "<td>" + val.name + "</td>" +
+                "<td>" + val.height + "</td>" +
+                "<td>" + val.mass + "</td>" +
+                "<td>" + val.hair_color + "</td>" +
+                "<td>" + val.gender + "</td>" +
+                "</tr>";
+        })
+        $("tbody").append(temp);
+    },
+    error: function (error) {
+        console.log(error)
+    }
+})*/
+
+/*
+function myFunction1() {
+    col1row1.style.backgroundColor = "lightblue";
+    col1row1.style.color = "steelblue";
+    col2row1.style.backgroundColor = "darkseagreen";
+    col2row1.style.color = "green";
+    col3row1.style.backgroundColor = "beige";
+    col3row1.style.color = "yellowgreen";
+}*/
+
+         
